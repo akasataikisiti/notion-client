032b353d76792a613086d46ed45291685861c543
akasataikisiti <nama0you0chu@gmail.com>
2023-02-18 18:13:20 +0900

    themeProviderで全体のレイアウトを決める

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AuthLayout from './components/layout/AuthLayout';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 function App() {
-  return <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<AuthLayout />}>
-        <Route path="login" element={<Login />} />
-        <Route path="register" element={<Register />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
+
+  const theme = createTheme({
+    palette: { mode: "dark" },
+  })
+
+  return (
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<AuthLayout />}>
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider >
+  )
 }
 
 export default App
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,7 @@ import AccessAlarmsIcon from '@mui/icons-material/AccessAlarms';
 const Login = () => {
   return (
     <div>
-      Login <AccessAlarmsIcon />
+      Login
     </div>
   )
 }
