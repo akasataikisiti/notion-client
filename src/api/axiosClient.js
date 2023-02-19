a51ace7be41af2b7cc4de975487c86edf086737e
akasataikisiti <nama0you0chu@gmail.com>
2023-02-19 11:34:15 +0900

    api叩く処理の前処理を実装
    api送信時にjwtトークンをlocalStrageから持ってくる部分を作成（localStrageにJWTトークン格納する機能は未実装）
    取得したjwtを httpヘッダのauthorization属性に `Bearer ${JWT}`の形で設置

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -4,7 +4,18 @@ const BASE_URL = "http://localhost:6001/api/v1/"
 
 const axiosClient = axios.create({
   baseURL: BASE_URL,
-
 });
+const getToken = () => localStorage.getItem("token")
+
+//APIを叩く前に前処理を行う
+axiosClient.interceptors.request.use(async (config) => {
+  return {
+    config,
+    header: {
+      "Content-Type": "application/json",
+      authorization: `Bearer ${getToken()}`,   //リクエストヘッダにJWTを付けてサーバに渡す
+    }
+  }
+})
 
 export default axiosClient;
