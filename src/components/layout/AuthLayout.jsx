a0b19d6564de42eb03b85a1780935842d11bdcc9
akasataikisiti <nama0you0chu@gmail.com>
2023-02-19 10:16:31 +0900

    AuthLaytout調整
    ロゴ挿入

--- src/components/layout/AuthLayout.jsx
+++ src/components/layout/AuthLayout.jsx
@@ -1,9 +1,25 @@
+import { Box } from "@mui/material"
+import { Container } from "@mui/system"
 import { Outlet } from "react-router-dom"
+import notionLogo from "../../assets/images/notion-logo-1.svg"
 
 const AuthLayout = () => {
   return (
     <div>
-      AuthLayout
+      <Container component="main" maxWidth="xs">
+        <Box sx={{
+          marginTop: 6,
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+        }}>
+          <img src={notionLogo}
+            alt=""
+            style={{ width: 100, height: 100, marginBottom: 3 }}
+          />
+          Notionクローン開発
+        </Box>
+      </Container>
       <Outlet />
     </div>
   )
