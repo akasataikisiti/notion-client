296289d6a5bb7186739126e3fb2896603a740f6d
akasataikisiti <nama0you0chu@gmail.com>
2023-02-20 02:26:50 +0900

    API送信中のローディングボタン機能実装

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -8,6 +8,7 @@ const Register = () => {
   const [usernameErrText, setUsernameErrText] = useState("")
   const [passwordErrText, setPasswordErrText] = useState("")
   const [confirmErrText, setConfirmErrText] = useState("")
+  const [loading, setLoading] = useState(false)
 
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -45,6 +46,8 @@ const Register = () => {
 
     if (error) return
 
+    setLoading(true)
+
     // 新規登録APIを叩く
     try {
       const res = await authApi.register({
@@ -52,6 +55,7 @@ const Register = () => {
         password,
         confirmPassword
       })
+      setLoading(false)
       localStorage.setItem("token", res.token)
       console.log("新規登録に成功しました")
     } catch (err) {
@@ -69,6 +73,7 @@ const Register = () => {
           setConfirmErrText(err.msg)
         }
       })
+      setLoading(false)
     }
   }
 
@@ -84,6 +89,7 @@ const Register = () => {
           required
           helperText={usernameErrText}
           error={usernameErrText !== ""}
+          disabled={loading}
         />
         <TextField
           fullWidth
@@ -95,6 +101,7 @@ const Register = () => {
           required
           helperText={passwordErrText}
           error={passwordErrText !== ""}
+          disabled={loading}
         />
         <TextField
           fullWidth
@@ -106,12 +113,13 @@ const Register = () => {
           required
           helperText={confirmErrText}
           error={confirmErrText !== ""}
+          disabled={loading}
         />
         <LoadingButton
           sx={{ mt: 3, mb: 2 }}
           fullWidth
           type="submit"
-          loading={false}
+          loading={loading}
           color="primary"
           variant="outlined"
         >
