1f9de56a2da7625f5654b345914e22b7bd7f7fdf
akasataikisiti <nama0you0chu@gmail.com>
2023-02-21 03:25:10 +0900

    メモAPIを叩いてメモを作成する

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,40 @@
+import { LoadingButton } from "@mui/lab"
+import { Box } from "@mui/material"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom";
+import memoApi from "../api/memoApi";
 
 export const Home = () => {
+  const navigate = useNavigate()
+  const [loading, setLoading] = useState(false)
+
+  const createMemo = async () => {
+    try {
+      setLoading(true)
+      const res = await memoApi.create()
+      console.log(res)
+      navigate(`/memo/${res._id}`)
+    } catch (err) {
+      console.log(err)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+
   return (
     <div >
-      home
+      <Box sx={{
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center"
+      }}>
+        <LoadingButton
+          valiant="outlined"
+          onClick={() => createMemo()}
+          loading={loading}>最初のメモを作成</LoadingButton>
+      </Box>
     </div >
   )
 }
