44298a8fed5eefe8a12240c8fb67dac8a813afcf
akasataikisiti <nama0you0chu@gmail.com>
2023-02-20 17:15:27 +0900

    アプリケーションレイアウト作成
    sidebarログアウトボタン追加

---
+++ src/components/common/Sidebar.jsx
@@ -0,0 +1,40 @@
+import {
+  Drawer,
+  IconButton,
+  List,
+  ListItemButton,
+  Typography,
+} from "@mui/material"
+import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined"
+import { Box } from "@mui/system"
+
+const Sidebar = () => {
+  return (
+    <Drawer
+      container={window.document.body}
+      variant="permanent"
+      open={true}
+      sx={{ width: 250, height: "100vh" }}
+    >
+      <List sx={{ width: 250, height: "100vh" }}>
+        <ListItemButton>
+          <Box sx={{
+            width: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between"
+          }}>
+            <Typography variant="body2" fontWeight="700">
+              kskabac
+            </Typography>
+            <IconButton>
+              <LogoutOutlinedIcon />
+            </IconButton>
+          </Box>
+        </ListItemButton>
+      </List>
+    </Drawer>
+  )
+}
+
+export default Sidebar
--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,7 +1,32 @@
-export const AppLayout = () => {
+import { Box } from "@mui/material"
+// import { Container } from "@mui/system"
+import { useEffect } from "react"
+import { Outlet, useNavigate } from "react-router-dom"
+import authUtils from "src/utils/authUtils"
+// import notionLogo from "../../assets/images/notion-logo-1.svg"
+import Sidebar from "../common/Sidebar"
+
+const AppLayout = () => {
+  const navigate = useNavigate()
+  useEffect(() => {
+    // JWTTを持っているのか確認
+    const checkAuth = async () => {
+      // 認証チェック
+      const user = await authUtils.isAuthenticated()
+      if (!user) {
+        navigate("/login")
+      }
+    }
+    checkAuth()
+  }, [navigate])
   return (
     <div>
-
+      <Box sx={{ display: "flex" }}>
+        <Sidebar />
+        <Box sx={{ flexGrow: 1, p: 1, width: "max-content" }}>
+          <Outlet />
+        </Box>
+      </Box>
     </div>
   )
 }
