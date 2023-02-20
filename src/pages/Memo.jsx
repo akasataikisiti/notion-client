0eab4c5800338e62aa4eab1899b5243bd4f802fd
akasataikisiti <nama0you0chu@gmail.com>
2023-02-21 05:15:23 +0900

    メモコンポーネント作成

--- src/pages/Memo.jsx
+++ src/pages/Memo.jsx
@@ -1,8 +1,31 @@
+import { IconButton, TextField } from "@mui/material"
+import { Box } from "@mui/system"
+import StarBorderOutlinedIcon from "@mui/icons-material/StarBorderOutlined"
+import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined"
+
 const memo = () => {
   return (
-    <div>
-      memo
-    </div>
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+          width: "100%",
+        }}
+      >
+        <IconButton>
+          <StarBorderOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <DeleteOutlinedIcon variant="outlined" color="error" />
+        </IconButton>
+      </Box>
+      <Box sx={{ padding: "10px 40px" }} >
+        <TextField placeholder="無題" variant="outlined" fullWidth />
+        <TextField placeholder="追加" variant="outlined" fullWidth />
+      </Box>
+    </>
   )
 }
 
