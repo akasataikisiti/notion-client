7ec984ef52f3271ce5d3034deec816db2780f163
akasataikisiti <nama0you0chu@gmail.com>
2023-02-21 20:47:55 +0900

    絵文字ピッカー修正
    大きさ修正
    外部で変更された絵文字選択を表示するようにする

--- src/components/common/EmojiPicker.jsx
+++ src/components/common/EmojiPicker.jsx
@@ -1,9 +1,19 @@
+import { Box, Typography } from "@mui/material"
+import { useEffect, useState } from "react"
 
 const EmojiPicker = (props) => {
+  const [selectedEmoji, setSelectedEmoij] = useState()
+
+  useEffect(() => {
+    setSelectedEmoij(props.icon)
+  }, [props.icon])
+
   return (
-    <div>
-      {props.icon}
-    </div>
+    <Box>
+      <Typography variant="h3" fontWeight="700" sx={{ cursor: "pointer" }}>
+        {selectedEmoji}
+      </Typography>
+    </Box>
   )
 }
 
